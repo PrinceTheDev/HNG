843e4da3e5ee700896d8f7cd3c642f54fb7bb154
PrinceTheDev <sinbadprince9@gmail.com>
2025-01-29 10:12:52 +0100

    Changed allowed hosts

--- public_api/settings.py
+++ public_api/settings.py
@@ -12,8 +12,10 @@ https://docs.djangoproject.com/en/5.1/ref/settings/
 
 from pathlib import Path
 import os
+from dotenv import load_dotenv
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
+load_dotenv()
 BASE_DIR = Path(__file__).resolve().parent.parent
 
 
@@ -26,7 +28,7 @@ SECRET_KEY = os.environ.get('DJANGO_SECRET_KEY')
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ["api.onrender.com"]
+ALLOWED_HOSTS = ["hng-1-zifm.onrender.com"]
 
 
 # Application definition
